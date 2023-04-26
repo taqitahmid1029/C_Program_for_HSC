f7a4dabffa703cafa8e504d9f5a38b1e17e4fd7b
Taqi Tahmid <131511564+taqitahmid1029@users.noreply.github.com>
2023-04-26 21:18:25 +0600

    Update and rename ৩টি সংখ্যার গড় to ৩টি সংখ্যার গড়.c

--- Basic/৩টি সংখ্যার গড়.c
+++ Basic/৩টি সংখ্যার গড়.c
@@ -2,7 +2,7 @@
 
 int main() {
     float a, b, c, avg;
-    printf("Enter values: ");
+    printf("Enter 3 integers: ");
     scanf("%f %f %f", &a, &b, &c);
     avg=(a+b+c)/3;
     printf("Average is %f", avg);
