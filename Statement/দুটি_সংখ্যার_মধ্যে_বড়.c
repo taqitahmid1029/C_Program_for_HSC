a56c74d1ad2dfed5d6c8d3e46221138c70f1b646
Taqi Tahmid <131511564+taqitahmid1029@users.noreply.github.com>
2023-04-26 22:21:48 +0600

    Update and rename দুটি সংখ্যার মধ্যে বড় to দুটি_সংখ্যার_মধ্যে_বড়.c

--- Statement/দুটি_সংখ্যার_মধ্যে_বড়.c
+++ Statement/দুটি_সংখ্যার_মধ্যে_বড়.c
@@ -3,7 +3,7 @@
 int main()
 {
     int a, b;
-    printf("Enter 2 numbers: ");
+    printf("Enter 2 integers: ");
     scanf("%d %d", &a, &b);
     if(a>b)
         printf("%d is greater number", a);
