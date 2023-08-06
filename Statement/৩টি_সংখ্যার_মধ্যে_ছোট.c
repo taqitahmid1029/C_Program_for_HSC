30a7bcdaa60c59a8c78f7aa085229a58367874bb
Taqi Tahmid <131511564+taqitahmid1029@users.noreply.github.com>
2023-08-06 07:00:45 +0600

    Update ৩টি_সংখ্যার_মধ্যে_ছোট.c

--- Statement/৩টি_সংখ্যার_মধ্যে_ছোট.c
+++ Statement/৩টি_সংখ্যার_মধ্যে_ছোট.c
@@ -5,17 +5,14 @@ int main()
     int a, b, c;
     printf("Enter 3 numbers: ");
     scanf("%d %d %d", &a, &b, &c);
-    if(a<b) {
-        if(a<c)
-            printf("%d is smallest number", a);
-        else
-            printf("%d is smallest number", c);
+    if(a<b && a<c) {
+        printf("%d is smallest number", a);
+    }
+    else if (b<a && b<c) {
+		printf("%d is smallest number", b);
     }
     else {
-        if(b<c)
-            printf("%d is smallest number", b);
-        else
-            printf("%d is smallest number", c);
+        printf("%d is smallest number", c);
     }
     return 0;
 }
