2ffb7447bc7b2536e5c5ee2db510d379bf147b80
Taqi Tahmid <131511564+taqitahmid1029@users.noreply.github.com>
2023-08-27 10:06:20 +0600

    Update অধিবর্ষ_সাল.c

--- Statement/অধিবর্ষ_সাল.c
+++ Statement/অধিবর্ষ_সাল.c
@@ -5,9 +5,14 @@ int main()
     int y;
     printf("Enter year: ");
     scanf("%d", &y);
-    if((y%400==0)||(y%100!=0 && y%4==0))
+    if(y%400==0) {
         printf("%d is leap year", y);
-    else
+    }
+    else if (y%100!=0 && y%4==0) {
+        printf("%d is leap year", y);
+    }
+    else {
         printf("%d is not leap year", y);
+    }
     return 0;
 }
